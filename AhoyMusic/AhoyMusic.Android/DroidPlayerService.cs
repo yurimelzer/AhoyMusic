a622878c6ab6a9f82ab4ea3279dd660cd8aee65e
Yuri Melzer Gonzaga <yurisosalimentos@hotmail.com>
2022-08-29 22:10:32 -0300

    Código sujeito e reset. Tentativa de fazer o player tocar background. Código não terminado

---
+++ AhoyMusic/AhoyMusic.Android/DroidPlayerService.cs
@@ -0,0 +1,41 @@
+﻿using AhoyMusic.DependecyServices;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AhoyMusic.Droid.DroidPlayerService))]
+namespace AhoyMusic.Droid
+{
+    [Activity]
+    public class DroidPlayerService : Activity, IPlayerService
+    {
+        public void InitPlayer()
+        {
+            var intent = new Intent(StreamingBackgroundService.ActionInitPlayer);
+            StartService(intent); // ERRO AQUI Fatal signal 11 (SIGSEGV)
+        }
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
